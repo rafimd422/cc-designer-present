c1c1f881ff9521387773c5e9e2464c1ecd345e07
rafimd422 <rafimd2222@gmail.com>
2024-01-10 00:39:50 +0600

    added the logo of cc on the webpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Card from '@/Components/Card'
 import { Inter } from 'next/font/google'
-
+import Image from 'next/image'
+import Logo from '@/Assets/CC-golden.png'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -8,7 +9,12 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center gap-4 py-8 p-2 ${inter.className}`}
     >
-<p className='primary'>Cultural Clasistists</p>
+<Image 
+alt='Cultural Classistists'
+height={3}
+width={400}
+className='my-8'
+src={Logo} />
 <p className='text-[1.125rem] my-1'>Description</p>
 <div className="flex flex-wrap md:container mx-auto gap-6 justify-center">
 <Card />
